e747f6c19d382f7f36e5d57a3b7d95ede44d066a
declankra <declankramper@gmail.com>
2024-02-23 16:15:08 -0600

    v2!!

--- Race Time Predictor/ContentView.swift
+++ Race Time Predictor/ContentView.swift
@@ -1,4 +1,123 @@
 import SwiftUI
+import HealthKit
+
+struct ContentView: View {
+    @State private var selectedDistanceIndex = 0
+    @State private var begDate = Date()
+    @State private var endDate = Date()
+    @State private var showingResults = false
+    @State private var predictionResult: String = ""
+    let raceDistances = ["5K", "10K", "Half Marathon", "Marathon"]
+    let predictDistances: [Double] = [5.0, 10.0, 21.0975, 42.195] // Distances in kilometers
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                if !showingResults {
+                    Form {
+                        Section(header: Text("Select your race distance")) {
+                            Picker("Race Distance", selection: $selectedDistanceIndex) {
+                                ForEach(0..<raceDistances.count, id: \.self) {
+                                    Text(self.raceDistances[$0])
+                                }
+                            }
+                        }
+                        Section(header: Text("Select training period for prediction")) {
+                            DatePicker("Beginning Date", selection: $begDate, in: ...Date(), displayedComponents: .date)
+                            DatePicker("End Date", selection: $endDate, in: ...Date(), displayedComponents: .date)
+                        }
+                        
+                        Button("Get My Prediction") {
+                            getMyPrediction()
+                        }.buttonStyle(.automatic)
+                    }
+                } else {
+                    // Results display
+                    VStack(spacing: 20) {
+                        
+                        Text(predictionResult)
+                            .font(.title)
+                        Button("New Prediction") {
+                            resetPrediction()
+                        }
+                        Link("Learn How This Works", destination: URL(string: "https://declankramper.notion.site/Race-Time-Predictor-App-6a485fdb13d84d07ab26e2aa7c3b2de0?pvs=4")!)
+                        Button("Share Results") {
+                            shareResults()
+                    }
+                    }
+                }
+            }
+            .navigationBarTitle("Race Time Predictor")
+            .navigationBarItems(trailing: Button(action: {
+                showingResults = false
+            }) {
+                if showingResults {
+                    Text("Edit")
+                }
+            })
+        }
+    }
+    
+    private func getMyPrediction() {
+        HealthKitManager.shared.requestAuthorization { success, error in
+            guard success else {
+                // Handle the error if authorization was not successful
+                DispatchQueue.main.async {
+                    self.predictionResult = "Error: \(error?.localizedDescription ?? "Could not access HealthKit data.")"
+                    self.showingResults = true // Show results area to display the error message
+                }
+                return
+            }
+            
+            // Assuming begDate and endDate are correctly set by the user's input
+            HealthKitManager.shared.getRunningWorkouts(begDate: self.begDate, endDate: self.endDate) { performances in
+                        let result = HealthKitManager.shared.findBestPerformance(workouts: performances, predictDistance: self.predictDistances[self.selectedDistanceIndex] * 1000)
+                        
+                        guard let bestPerformance = result.bestPerformance, let lowestPredictedTime = result.lowestPredictedTime else {
+                            DispatchQueue.main.async {
+                                self.predictionResult = "No workouts found in the specified period."
+                                self.showingResults = true
+                            }
+                            return
+                        }
+            
+                // Display the best performance for reference
+                    // let bestTime = bestPerformance.time
+                    // let bestDistance = bestPerformance.distance
+                    // let bestDate = bestPerformance.date // need to add date to bestPerformance by first adding to Performance structure
+                
+                DispatchQueue.main.async {
+                               self.predictionResult = "You are predicted to run a \(self.raceDistances[self.selectedDistanceIndex]) in \(self.formatTime(lowestPredictedTime))"
+                               self.showingResults = true
+                           }
+                }
+            }
+        
+    }
+
+    
+    private func resetPrediction() {
+        self.showingResults = false
+        self.selectedDistanceIndex = 0
+        self.begDate = Date()
+        self.endDate = Date()
+        self.predictionResult = ""
+    }
+    
+    private func formatTime(_ time: TimeInterval) -> String {
+           let hours = Int(time) / 3600
+           let minutes = (Int(time) % 3600) / 60
+           let seconds = Int(time) % 60
+           return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+       }
+    
+    private func shareResults() {
+        // The actual sharing functionality requires UIKit integration
+        // Use UIActivityViewController in UIKit to share the results
+        // This part of the code cannot be directly implemented in SwiftUI without using UIViewControllerRepresentable
+        print("Share functionality to be implemented with UIActivityViewController")
+    }
+}
 
 // preview in canvas
 struct ContentView_Previews: PreviewProvider {
@@ -6,86 +125,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-struct ContentView: View {
-    @StateObject private var healthKitManager = HealthKitManager()
-    @State private var selectedDistanceIndex = 0
-    let raceDistances = ["5K", "10K", "Half Marathon", "Marathon"]
-    @State private var begDate = Date()
-    @State private var endDate = Date()
-    
-    var body: some View {
-        NavigationView {
-            Form {
-                Section(header: Text("Enter Your Race Distance")) {
-                    Picker("Select distance:", selection: $selectedDistanceIndex) {
-                        ForEach(0 ..< raceDistances.count, id: \.self) {
-                            Text(self.raceDistances[$0])
-                        }
-                    }.pickerStyle(SegmentedPickerStyle())
-                }
-                
-                Section(header: Text("Select Training Period for Prediction")) {
-                    DatePicker("Beginning Date", selection: $begDate, displayedComponents: .date)
-                    DatePicker("End Date", selection: $endDate, displayedComponents: .date)
-                }
-                
-                Section {
-                    Button("Get My Prediction") {
-                        print("Button tapped")
-                        healthKitManager.requestAuthorization { authorized in
-                            DispatchQueue.main.async {
-                                if authorized {
-                                    self.healthKitManager.predictDistance = distanceForSelectedIndex(self.selectedDistanceIndex)
-                                    self.healthKitManager.begDate = self.begDate
-                                    self.healthKitManager.endDate = self.endDate
-                                    self.healthKitManager.getHealthData()
-                                } else {
-                                    print("HealthKit authorization was denied by the user.")
-                                }
-                            }
-                        }
-                    }
-                }
-                
-                // Results display section
-                if healthKitManager.closestPerf != nil {
-                    Section(header: Text("Prediction")) {
-                        // Access predictedTime through healthKitManager
-                        Text("You are predicted to run a \(raceDistances[selectedDistanceIndex]) in \(formatTime(healthKitManager.predictedTime ?? 0))")
-                    }
-                }
-            }
-            .navigationBarTitle("Race Time Predictor")
-            
-        }
-    }
-    
-    private func distanceForSelectedIndex(_ index: Int) -> Double {
-            // Convert the selected distance to meters
-            switch raceDistances[index] {
-                case "5K":
-                    return 5000 // meters
-                case "10K":
-                    return 10000 // meters
-                case "Half Marathon":
-                    return 21097.5 // meters
-                case "Marathon":
-                    return 42195 // meters
-                default:
-                    return 0 // meters
-            }
-        }
-    
-    
-        
-    private func formatTime(_ time: TimeInterval) -> String {
-            // Format the time for display
-            let hours = Int(time) / 3600
-            let minutes = Int(time) % 3600 / 60
-            let seconds = Int(time) % 60
-            return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
-        }
-    }
-
-
--- Race Time Predictor/HealthKitManager.swift
+++ Race Time Predictor/HealthKitManager.swift
@@ -1,113 +1,87 @@
-//
-//  HealthKitManager.swift
-//  Race Time Predictor
-//
-//  Created by Declan Kramper on 2/22/24.
-//
-
+import Foundation
 import HealthKit
-import SwiftUI
 
-struct Performance: Identifiable {
-    let id = UUID()
-    var time: TimeInterval
-    var distance: Double
-}
+class HealthKitManager {
+    static let shared = HealthKitManager()
+    private var healthStore: HKHealthStore?
 
-class HealthKitManager: ObservableObject {
-    private let healthStore = HKHealthStore()
-    @Published var predictDistance: Double = 0 // user selected distance
-    @Published var begDate: Date? // start date of health data retrieval
-    @Published var endDate: Date? // last date of health data retrieval
-    @Published var closestPerf: Performance? // performance used for calculation
-    @Published var longestDist: Double = 0 // stores longest distance
-    @Published var predictedTime: TimeInterval? // holds the predicted time result
-
-    // Request HealthKit permission
-    func requestAuthorization(completion: @escaping (Bool) -> Void) {
-        let typesToShare: Set = [HKQuantityType.workoutType()]
-        let typesToRead: Set = [HKQuantityType.workoutType()]
-        healthStore.requestAuthorization(toShare: typesToShare, read: typesToRead) { success, error in
-            DispatchQueue.main.async {
-                if let error = error {
-                    print("HealthKit authorization error: \(error.localizedDescription)")
-                }
-                print("HealthKit authorization success: \(success)")
-                completion(success)
-            }
+    init() {
+        if HKHealthStore.isHealthDataAvailable() {
+            healthStore = HKHealthStore()
         }
     }
-    
-    // Get health data from HealthKit
-    func getHealthData() {
-        guard let begDate = begDate, let endDate = endDate else {
-            print("Begin or end date is nil")
+
+    func requestAuthorization(completion: @escaping (Bool, Error?) -> Void) {
+        guard let healthStore = healthStore else {
+            completion(false, NSError(domain: "com.example.HealthKitManager", code: 2, userInfo: [NSLocalizedDescriptionKey: "HealthKit is not available in this device."]))
             return
         }
-        let sampleType = HKSampleType.workoutType()
-        
-        // Define the predicate to filter workouts by date and by running activity type
-        let datePredicate = HKQuery.predicateForSamples(withStart: begDate, end: endDate, options: .strictStartDate)
-        let runningPredicate = HKQuery.predicateForWorkouts(with: .running)
-        let combinedPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [datePredicate, runningPredicate])
-        
-        let query = HKSampleQuery(sampleType: sampleType, predicate: combinedPredicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { [weak self] (_, samples, error) in
-            guard let self = self else {
-                print("Self is nil")
-                return
-            }
-            
-            if let error = error {
-                print("An error occurred fetching the user's workouts: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let workouts = samples as? [HKWorkout], !workouts.isEmpty else {
-                print("No workouts found or not a workout sample")
-                return
-            }
-            
-            print("Fetched \(workouts.count) workouts of type 'Running'")
-            self.findClosestPerf(workouts: workouts)
+
+        let typesToRead: Set<HKObjectType> = [
+            HKObjectType.workoutType()
+        ]
+
+        healthStore.requestAuthorization(toShare: nil, read: typesToRead) { success, error in
+            completion(success, error)
         }
-        
+    }
+
+    func getRunningWorkouts(begDate: Date, endDate: Date, completion: @escaping ([Performance]) -> Void) {
+        guard let healthStore = healthStore else {
+            completion([])
+            return
+        }
+
+        let runningPredicate = HKQuery.predicateForWorkouts(with: .running)
+        let datePredicate = HKQuery.predicateForSamples(withStart: begDate, end: endDate, options: .strictStartDate)
+        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [runningPredicate, datePredicate])
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: true)
+
+        let query = HKSampleQuery(sampleType: HKObjectType.workoutType(), predicate: compoundPredicate, limit: HKObjectQueryNoLimit, sortDescriptors: [sortDescriptor]) { _, samples, _ in
+            guard let workouts = samples as? [HKWorkout] else {
+                completion([])
+                return
+            }
+
+            let performances = workouts.map {
+                Performance(id: $0.uuid, time: $0.duration, distance: $0.totalDistance?.doubleValue(for: .meter()) ?? 0)
+            }
+            completion(performances)
+        }
+
         healthStore.execute(query)
     }
 
-    
-    // Find the closest performance
-    private func findClosestPerf(workouts: [HKWorkout]) {
-            DispatchQueue.global(qos: .userInitiated).async {
-                var bestPerformance: Performance?
-                workouts.forEach { workout in
-                    let distance = workout.totalDistance?.doubleValue(for: .meter()) ?? 0
-                    if distance > self.longestDist && distance < self.predictDistance {
-                        self.longestDist = distance
-                        bestPerformance = Performance(time: workout.duration, distance: distance)
-                    }
-                }
-                
-                DispatchQueue.main.async {
-                    if let bestPerf = bestPerformance {
-                        print("Found closest performance: \(bestPerf)")
-                        self.closestPerf = bestPerf
-                        self.predictedTime = self.calculateTime(distance: self.predictDistance, closestPerf: bestPerf)
-                        print("Predicted time: \(self.predictedTime ?? -1)")
-                    } else {
-                        print("No closest performance found")
-                    }
-                }
+    func findBestPerformance(workouts: [Performance], predictDistance: Double) -> (bestPerformance: Performance?, lowestPredictedTime: Double?) {
+        guard !workouts.isEmpty else { return (nil, nil) }
+
+        var bestPerformance: Performance?
+        var lowestPredictedTime: Double?
+
+        for workout in workouts {
+            let predictedTime = calculatePredictedTime(for: workout, predictDistance: predictDistance)
+            if lowestPredictedTime == nil || predictedTime < lowestPredictedTime! {
+                lowestPredictedTime = predictedTime
+                bestPerformance = workout
             }
         }
-    
-    // Calculate predicted time using Pete Riegel’s formula
-        func calculateTime(distance predictDistance: Double, closestPerf: Performance) -> TimeInterval {
-            let T1 = closestPerf.time
-            let D1 = closestPerf.distance
-            let D2 = predictDistance
-            
-            // Pete Riegel’s formula: T2 = T1 * (D2/D1) ^ 1.06
-            let T2 = T1 * pow((D2/D1), 1.06)
-            return T2
-        }
+        
+        return (bestPerformance, lowestPredictedTime)
     }
+
+
+    func calculatePredictedTime(for performance: Performance, predictDistance: Double) -> Double {
+        // Pete Riegel’s formula: T2 = T1 * (D2 / D1) ^ 1.06
+        let T1 = performance.time
+        let D1 = performance.distance
+        let D2 = predictDistance
+
+        return T1 * pow((D2 / D1), 1.06)
+    }
+}
+
+struct Performance {
+    var id: UUID
+    var time: Double // Assuming time is in seconds
+    var distance: Double // Assuming distance is in meters
+}
