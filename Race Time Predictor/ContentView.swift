b3ae38609ae4d7fc907515689d31922751cae333
declankra <declankramper@gmail.com>
2024-09-16 17:21:11 -0500

    results UI updates and pace required calcs

--- Race Time Predictor/ContentView.swift
+++ Race Time Predictor/ContentView.swift
@@ -6,37 +6,43 @@ struct ContentView: View {
     @State private var begDate = Date()
     @State private var endDate = Date()
     @State private var showingResults = false
-    @State private var predictionResult: String = ""
-    @State private var bestPerformanceDetails: String = "" // for displaying the healthkit data
-    @State private var showBestPerformanceDetails: Bool = false // for toggling details display
+    @State private var predictedTime: TimeInterval? = nil
+    @State private var requiredPace: Double? = nil
+    @State private var bestPerformanceDetails: String = ""
+    @State private var showBestPerformanceDetails: Bool = false
     let raceDistances = ["5K", "10K", "Half Marathon", "Marathon"]
-    let predictDistances: [Double] = [5.0, 10.0, 21.0975, 42.195] // Distances in kilometers
-    @State private var showingShareSheet = false // Add a state variable to control the presentation of the share view
+    let predictDistances: [Double] = [5.0, 10.0, 21.0975, 42.195]
+    @State private var showingShareSheet = false
     @State private var shareTime = "null"
-    @State private var showingFeedbackView = false // Add a state variable to control the presentation of the feedback view
-    @State private var showingHealthKitInfoView = false // new state variable to control the presentation of the HealthKit info view
-    
-    
-    var body: some View {
-           NavigationView {
-               ScrollView {
-                   VStack(alignment: .leading, spacing: 70) {
-                       if !showingResults {
-                           raceDistanceSection
-                           trainingPeriodSection
-                           getPredictionButton
-                           healthKitInfoLink
+    @State private var showingFeedbackView = false
+    @State private var showingHealthKitInfoView = false
+    @State private var errorMessage: String? = nil
 
-                       } else {
-                           resultsSection
-                       }
-                   }
-                   .padding()
-               }
-               .navigationBarTitle("Race Time Predictor")
-               .overlay(feedbackButton, alignment: .bottomTrailing)
-           }
-       }
+
+    var body: some View {
+            NavigationView {
+                ZStack(alignment: .bottomTrailing) {
+                    ScrollView {
+                        VStack(alignment: .leading, spacing: 70) {
+                            if !showingResults {
+                                raceDistanceSection
+                                trainingPeriodSection
+                                getPredictionButton
+                                healthKitInfoLink
+                            } else {
+                                resultsSection
+                            }
+                        }
+                        .padding()
+                        .padding(.bottom, 90) // Adjust padding as needed
+                    }
+
+                    feedbackButton
+                        .padding()
+                }
+                .navigationBarTitle("Race Time Predictor")
+            }
+        }
        
     var raceDistanceSection: some View {
         VStack(alignment: .leading, spacing: 5) {
@@ -83,123 +89,202 @@ struct ContentView: View {
            }
        }
        
-       var resultsSection: some View {
-           VStack(spacing: 20) {
-               Spacer()
-               
-               Text(predictionResult)
-                   .font(.title)
-               
-               Spacer()
-               
-               if !bestPerformanceDetails.isEmpty {
-                   Button(action: {
-                       withAnimation {
-                           showBestPerformanceDetails.toggle()
-                       }
-                   }) {
-                       Text("Show best workout performance from HealthKit used to calculate your prediction")
-                           .foregroundColor(.blue)
-                   }
-                   if showBestPerformanceDetails {
-                       Text(bestPerformanceDetails)
-                           .transition(.scale)
-                           .fixedSize(horizontal: false, vertical: true)
-                   }
-               } else if predictionResult.contains("No running workouts found") {
-                   // Add the fix instructions here
-                   VStack(alignment: .leading, spacing: 10) {
-                       Text("To fix:")
-                           .font(.headline)
-                       Text("(1) Check dates for training period")
-                       Text("(2) Ensure there are running workouts in your Apple Health App")
-                       Text("(3) Grant our app permission to access your Apple Health App workouts in settings")
-                   }
-                   .padding()
-                   .background(Color.gray.opacity(0.1))
-                   .cornerRadius(10)
-               }
-               
-               Spacer()
-               Spacer()
-               Spacer()
-
-               Button("New Prediction") {
-                   resetPrediction()
-               }
-               Link("Learn How This Works", destination: URL(string: "https://declankramper.notion.site/Race-Time-Predictor-App-6a485fdb13d84d07ab26e2aa7c3b2de0?pvs=4")!)
-               Button("Share Results") {
-                   self.showingShareSheet = true
-               }
-               .sheet(isPresented: $showingShareSheet) {
-                   ActivityView(activityItems: [self.shareMessage()])
-               }
-           }
-       }
-       
-       var feedbackButton: some View {
-           Button(action: {
-               self.showingFeedbackView = true
-           }) {
-               Image(systemName: "info.circle")
-                   .font(.largeTitle)
-                   .foregroundColor(.blue)
-           }
-           .padding()
-           .sheet(isPresented: $showingFeedbackView) {
-               FeedbackView()
-           }
-       }
-    
-    private func getMyPrediction() {
-        HealthKitManager.shared.requestAuthorization { success, error in
-            guard success else {
-                // Handle the error if authorization was not successful
-                DispatchQueue.main.async {
-                    self.predictionResult = "Error: \(error?.localizedDescription ?? "Could not access HealthKit data.")"
-                    self.showingResults = true // Show results area to display the error message
+    var resultsSection: some View {
+        VStack(spacing: 30) {
+            Spacer()
+            
+            if let predictedTime = predictedTime, let requiredPace = requiredPace, errorMessage == nil {
+                // Display prediction results
+                VStack(spacing: 20) {
+                    Text("\(raceDistances[selectedDistanceIndex]) Prediction")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Text(formatTime(predictedTime))
+                        .font(.system(size: 58, weight: .heavy))
+                        .foregroundColor(.green)
+                    
+                    Text("Pace Required")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                    
+                    Text("\(formatPace(requiredPace)) min/mile")
+                        .font(.system(size: 34, weight: .semibold))
+                        .foregroundColor(.green)
                 }
-                return
+                .multilineTextAlignment(.center)
+            } else if let errorMessage = errorMessage {
+                // Display error message
+                Text(errorMessage)
+                    .font(.title2)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            } else {
+                // No data case
+                Text("No running workouts found in the specified period.")
+                    .font(.title2)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
+                        
+            if !bestPerformanceDetails.isEmpty {
+                Button(action: {
+                    withAnimation {
+                        showBestPerformanceDetails.toggle()
+                    }
+                }) {
+                    Text(showBestPerformanceDetails ? "Hide Details" : "Show best workout performance used to calculate prediction")
+                        .foregroundColor(.blue)
+                        .font(.system(size: 15))
+                }.padding(.top, 10)
+                if showBestPerformanceDetails {
+                    Text(bestPerformanceDetails)
+                        .padding()
+                        .background(Color(UIColor.systemGray6))
+                        .cornerRadius(10)
+                        .transition(.opacity)
+                }
             }
             
-            // Assuming begDate and endDate are correctly set by the user's input, get the result from findBestPerformance
-            HealthKitManager.shared.getRunningWorkouts(begDate: self.begDate, endDate: self.endDate) { performances in
-                let result = HealthKitManager.shared.findBestPerformance(workouts: performances, predictDistance: self.predictDistances[self.selectedDistanceIndex] * 1000)
-                // Handle results display
-                DispatchQueue.main.async {
-                    if let bestPerformance = result.bestPerformance, let lowestPredictedTime = result.lowestPredictedTime {
-                        self.predictionResult = "You are predicted to run a \(self.raceDistances[self.selectedDistanceIndex]) in:  \(self.formatTime(lowestPredictedTime))"
-                        self.shareTime = self.formatTime(lowestPredictedTime)
-                        self.bestPerformanceDetails = "Best Performance\nDate: \(bestPerformance.date.formatted(date: .abbreviated, time: .shortened))\nTime: \(formatTime(bestPerformance.time))\nDistance: \(formatDistance(bestPerformance.distance)) miles"
-                    } else {
-                        self.predictionResult = "No running workouts found in the specified period."
-
+            Spacer()
+            
+            // Conditional display of buttons
+            if predictedTime != nil && requiredPace != nil && errorMessage == nil {
+                // Display both buttons
+                HStack(spacing: 20) {
+                    Button(action: {
+                        resetPrediction()
+                    }) {
+                        Text("New Prediction")
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
                     }
-                    self.showingResults = true
+                    
+                    Button(action: {
+                        self.showingShareSheet = true
+                    }) {
+                        Text("Share Results")
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    .sheet(isPresented: $showingShareSheet) {
+                        ActivityView(activityItems: [self.shareMessage()])
+                    }
                 }
-                
+            } else {
+                // Display only the "New Prediction" button
+                Button(action: {
+                    resetPrediction()
+                }) {
+                    Text("New Prediction")
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal)
+            }
+            
+            Link("Learn How This Works", destination: URL(string: "https://declankramper.notion.site/Race-Time-Predictor-App-6a485fdb13d84d07ab26e2aa7c3b2de0?pvs=4")!)
+                .padding(.top, 10)
+        }
+        .padding()
+    }
+    
+       
+    var feedbackButton: some View {
+            Button(action: {
+                self.showingFeedbackView = true
+            }) {
+                Image(systemName: "info.circle")
+                    .font(.largeTitle)
+                    .foregroundColor(.blue)
+            }
+            .sheet(isPresented: $showingFeedbackView) {
+                FeedbackView()
             }
         }
-        
-    }
+    
+    private func getMyPrediction() {
+            HealthKitManager.shared.requestAuthorization { success, error in
+                guard success else {
+                    DispatchQueue.main.async {
+                        self.predictedTime = nil
+                        self.requiredPace = nil
+                        self.errorMessage = "Error: \(error?.localizedDescription ?? "Could not access HealthKit data.")"
+                        self.showingResults = true
+                    }
+                    return
+                }
+                
+                HealthKitManager.shared.getRunningWorkouts(begDate: self.begDate, endDate: self.endDate) { performances in
+                    let result = HealthKitManager.shared.findBestPerformance(workouts: performances, predictDistance: self.predictDistances[self.selectedDistanceIndex] * 1000)
+                    
+                    DispatchQueue.main.async {
+                        if let bestPerformance = result.bestPerformance, let lowestPredictedTime = result.lowestPredictedTime {
+                            self.predictedTime = lowestPredictedTime
+                            self.shareTime = self.formatTime(lowestPredictedTime)
+                            self.bestPerformanceDetails = """
+                            Date: \(bestPerformance.date.formatted(date: .abbreviated, time: .omitted))
+                            Time: \(formatTime(bestPerformance.time))
+                            Distance: \(formatDistance(bestPerformance.distance)) miles
+                            """
+                            
+                            // Calculate pace in minutes per mile
+                            let predictedDistanceMiles = (self.predictDistances[self.selectedDistanceIndex] * 1000) * 0.000621371
+                            let pace = lowestPredictedTime / 60.0 / predictedDistanceMiles
+                            self.requiredPace = pace
+                            self.errorMessage = nil
+                        } else {
+                            self.predictedTime = nil
+                            self.requiredPace = nil
+                            self.errorMessage = "No running workouts found in the specified period."
+                        }
+                        self.showingResults = true
+                    }
+                }
+            }
+        }
     
     
     private func resetPrediction() {
-        self.showingResults = false
-        self.selectedDistanceIndex = 0
-        self.predictionResult = ""
-        self.shareTime = "[null]"
-        self.bestPerformanceDetails = ""
-        self.showBestPerformanceDetails = false
-    }
+            self.showingResults = false
+            self.selectedDistanceIndex = 0
+            self.predictedTime = nil
+            self.requiredPace = nil
+            self.errorMessage = nil
+            self.shareTime = "[null]"
+            self.bestPerformanceDetails = ""
+            self.showBestPerformanceDetails = false
+        }
     
-    // Function to format the time for result display
-    private func formatTime(_ time: TimeInterval) -> String {
-        let hours = Int(time) / 3600
-        let minutes = (Int(time) % 3600) / 60
-        let seconds = Int(time) % 60
-        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
-    }
+    // Helper functions
+        private func formatTime(_ time: TimeInterval) -> String {
+            let hours = Int(time) / 3600
+            let minutes = (Int(time) % 3600) / 60
+            let seconds = Int(time) % 60
+            if hours > 0 {
+                return String(format: "%d:%02d:%02d", hours, minutes, seconds)
+            } else {
+                return String(format: "%02d:%02d", minutes, seconds)
+            }
+        }
+        
+        private func formatPace(_ pace: Double) -> String {
+            let paceMinutes = Int(pace)
+            let paceSeconds = Int((pace - Double(paceMinutes)) * 60.0 + 0.5)
+            return String(format: "%d:%02d", paceMinutes, paceSeconds)
+        }
     
     // Function to format the distance for result display
     func formatDistance(_ meters: Double) -> String {
